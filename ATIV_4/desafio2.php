3035a91eee1067a79d6ff1a802ed0b31954f95f3
Geandra <144291147+cantarino2@users.noreply.github.com>
2024-10-24 21:06:54 -0300

    Add files via upload

---
+++ ATIV_4/desafio2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Números de 100 a 200 com Incremento de 2</title>
+</head>
+<body>
+    <h1>Números de 100 a 200 com Incremento de 2</h1>
+    
+    <?php
+    for ($i = 100; $i <= 200; $i += 2) {
+        echo $i . "<br>";
+    }
+    ?>
+</body>
+</html>
+
---
+++ ATIV_4/desafio3.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Números Ímpares de 500 a 1000</title>
+</head>
+<body>
+    <h1>Números Ímpares de 500 a 1000</h1>
+    
+    <?php
+    for ($i = 501; $i <= 1000; $i += 2) { // Começa em 501, o primeiro ímpar após 500
+        echo $i . "<br>";
+    }
+    ?>
+</body>
+</html>
+
